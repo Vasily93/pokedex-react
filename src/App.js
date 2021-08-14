bb4561661a6991020b97fc5db8eb4fac58600dfd
Vasily93 <markovavasily@gmail.com>
2021-08-14 00:19:45 -0700

    rendering pokemons imgs and maes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
-
+import pokedex from './pokedex';
+import Pokecard from './Pokecard';
 function App() {
   return (
     <div>
       <h1>Pokedex App</h1>
+      <ul>{pokedex.map(p => <Pokecard p={p}/>)}</ul>
     </div>
   );
 }
---
+++ src/Pokecard.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Pokecard.css';
+
+const getId = (id) => {
+    const arr = id.toString().split('');
+    while(arr.length < 3) {
+        arr.unshift("0");
+    }
+    const res =  arr.join('');
+    return `https://assets.pokemon.com/assets/cms2/img/pokedex/detail/${res}.png`
+}
+
+
+class Pokecard extends React.Component {
+    render() {
+        const pokemon = this.props.p;
+        const imgUrl = getId(pokemon.id)
+        return (
+            <li key={pokemon.id}>
+                Pokemon: {pokemon.name}
+                <img alt="pokemon" src={imgUrl}/>
+            </li>
+        );
+    }
+}
+
+export default Pokecard;
